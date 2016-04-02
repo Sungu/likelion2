26dd7a39d7aa6754f4581600ed64d047d3e9cd53
Sungu Nam <naisz@naver.com>
2016-04-02 05:11:38 +0000

    bonus

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
     def index
-        @number = (1..45).to_a.sample(6).sort
+        @number = (1..45).to_a.sample(7).sort
+        @bonus = @number.sample
+        @main = @number-[@bonus]
     end
 end
